28f72bc1999c33c2f7710a131ada90d6e3ee54b5
Prashant Verma <pverma@digitalriver.com>
2019-10-29 16:18:43 -0500

    add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.currently.temperature)
+            callback(undefined, body.currently.temperature + ' High today: ' + body.daily.data[0].temperatureHigh + ' Low today: ' + body.daily.data[0].temperatureLow)
         }
     })
 }
